baa6f423a2ae20d61e28bdef4ee4e14716050cd7
Camille <gonzcami@oregonstate.edu>
2023-11-14 14:07:12 -0800

    nodejs starter step 4

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 var express = require('express');                   // We are using the express library for the web server
 var app     = express();                            // We need to instantiate an express object to interact with the server in our code
 const { engine } = require('express-handlebars');
-var exphbs = require('express-handlebars');         // Import express-handlebars
+var exphbs  = require('express-handlebars');         // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));      // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                     // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 var db      = require('./database/db-connector')    // Database
@@ -16,9 +16,14 @@ PORT        = 4200;                                 // Set a port number at the 
     ROUTES
 */
 app.get('/', function(req, res)
-    {
-        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
-    });                                         // will process this file, before sending the finished HTML to the client.
+    {  
+        let query1 = "SELECT * FROM bsg_people;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query                             
 
 /*
     LISTENER
