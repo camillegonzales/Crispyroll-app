8f4dabf963419dc3d5eab096c70f4c631cfecbca
Camille <gonzcami@oregonstate.edu>
2023-11-14 17:04:24 -0800

    nodejs starter step 8

--- public/js/add_person.js
+++ public/js/add_person.js
@@ -56,8 +56,6 @@ addPersonForm.addEventListener("submit", function (e) {
 })
 
 
-// Creates a single row from an Object representing a single record from 
-// bsg_people
 addRowToTable = (data) => {
 
     // Get a reference to the current table on the page and clear it out.
@@ -78,6 +76,8 @@ addRowToTable = (data) => {
     let homeworldCell = document.createElement("TD");
     let ageCell = document.createElement("TD");
 
+    let deleteCell = document.createElement("TD");
+
     // Fill the cells with correct data
     idCell.innerText = newRow.id;
     firstNameCell.innerText = newRow.fname;
@@ -85,13 +85,34 @@ addRowToTable = (data) => {
     homeworldCell.innerText = newRow.homeworld;
     ageCell.innerText = newRow.age;
 
+    deleteCell = document.createElement("button");
+    deleteCell.innerHTML = "Delete";
+    deleteCell.onclick = function(){
+        deletePerson(newRow.id);
+    };
+
     // Add the cells to the row 
     row.appendChild(idCell);
     row.appendChild(firstNameCell);
     row.appendChild(lastNameCell);
     row.appendChild(homeworldCell);
     row.appendChild(ageCell);
+    row.appendChild(deleteCell);
     
+    // Add a custom row attribute so the deleteRow function can find a newly added row
+    row.setAttribute('data-value', newRow.id);
+
     // Add the row to the table
     currentTable.appendChild(row);
+
+    // Start of new Step 8 code for adding new data to the dropdown menu for updating people
+    
+    // Find drop down menu, create a new option, fill data in the option (full name, id),
+    // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
+    let selectMenu = document.getElementById("mySelect");
+    let option = document.createElement("option");
+    option.text = newRow.fname + ' ' +  newRow.lname;
+    option.value = newRow.id;
+    selectMenu.add(option);
+    // End of new step 8 code.
 }
--- public/js/delete_person.js
+++ public/js/delete_person.js
@@ -38,3 +38,29 @@ function deleteRow(personID){
        }
     }
 }
+
+
+function deleteRow(personID){
+
+    let table = document.getElementById("people-table");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+       //iterate through rows
+       //rows would be accessed using the "row" variable assigned in the for loop
+       if (table.rows[i].getAttribute("data-value") == personID) {
+            table.deleteRow(i);
+            deleteDropDownMenu(personID);
+            break;
+       }
+    }
+}
+
+function deleteDropDownMenu(personID){
+  let selectMenu = document.getElementById("mySelect");
+  for (let i = 0; i < selectMenu.length; i++){
+    if (Number(selectMenu.options[i].value) === Number(personID)){
+      selectMenu[i].remove();
+      break;
+    } 
+
+  }
+}
