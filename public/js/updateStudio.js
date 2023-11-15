08c64eec42065d0a177cbea863f8e668f2954c53
jct102 <122237723+jct102@users.noreply.github.com>
2023-11-15 14:58:04 -0500

    fixes

--- public/js/updateStudio.js
+++ public/js/updateStudio.js
@@ -8,17 +8,17 @@ updateStudioForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
     // Get form fields we need to get data from
-    let inputStudioID = document.getElementById("mySelect");
-    let inputStudioName = document.getElementById("input-studio-name");
+    let inputStudioName = document.getElementById("mySelect");
+    let inputYearFounded = document.getElementById("year_founded");
 
     // Get the values from the form fields
-    let studioIDValue = inputStudioID.value;
     let studioNameValue = inputStudioName.value;
+    let yearFoundedValue = inputYearFounded.value;
     
     // currently the database table for bsg_people does not allow updating values to NULL
     // so we must abort if being bassed NULL for homeworld
 
-    if (isNaN(studioIDValue)) 
+    if (isNaN(studioNameValue)) 
     {
         return;
     }
@@ -26,8 +26,8 @@ updateStudioForm.addEventListener("submit", function (e) {
 
     // Put our data we want to send in a javascript object
     let data = {
-        studio_id: studioIDValue,
         studio_name: studioNameValue,
+        year_founded: yearFoundedValue,
     }
     
     // Setup our AJAX request
@@ -40,7 +40,7 @@ updateStudioForm.addEventListener("submit", function (e) {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
 
             // Add the new data to the table
-            updateRow(xhttp.response, fullNameValue);
+            updateRow(xhttp.response, studioNameValue);
 
         }
         else if (xhttp.readyState == 4 && xhttp.status != 200) {
@@ -74,4 +74,51 @@ function updateRow(data, personID){
             td.innerHTML = parsedData[0].name; 
        }
     }
+}
+
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("studios-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let studioIDCell = document.createElement("TD");
+    let studioNameCell = document.createElement("TD");
+    let yearFoundedCell = document.createElement("TD");
+    let deleteCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    studioIDCell.innerText = newRow.id;
+    studioNameCell.innerText = newRow.fname;
+    yearFoundedCell.innerText = newRow.lname;
+    deleteCell.innerText = newRow.homeworld;
+
+    // Add the cells to the row 
+    row.appendChild(studioIDCell);
+    row.appendChild(studioNameCell);
+    row.appendChild(yearFoundedCell);
+    row.appendChild(deleteCell);
+
+    // Add a custom row attribute so the deleteRow function can find a newly added row
+    row.setAttribute('data-value', newRow.studio_id);
+
+    // Add the row to the table
+    currentTable.appendChild(row);
+
+    // Find drop down menu, create a new option, fill data in the option (full name, id),
+    // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
+    let selectMenu = document.getElementById("mySelect");
+    let option = document.createElement("option");
+    option.text = newRow.studio_id;
+    option.value = newRow.studio_id;
+    selectMenu.add(option);
+    // End of new step 8 code.
 }
