07c2d5ff743df4bc274f36ff1f1c49c21526a503
Camille <gonzcami@oregonstate.edu>
2023-11-16 00:19:43 -0800

    removed addrowtotable from updatejs

--- public/js/updateStudio.js
+++ public/js/updateStudio.js
@@ -9,7 +9,7 @@ updateStudioForm.addEventListener("submit", function (e) {
 
     // Get form fields we need to get data from
     let inputStudioName = document.getElementById("mySelect");
-    let inputYearFounded = document.getElementById("year_founded_update");
+    let inputYearFounded = document.getElementById("year-founded-update");
 
     // Get the values from the form fields
     let studioNameValue = inputStudioName.value;
@@ -41,7 +41,6 @@ updateStudioForm.addEventListener("submit", function (e) {
 
             // Add the new data to the table
             updateRow(xhttp.response, studioNameValue);
-            // addRowToTable(xhttp.response);
 
         }
         else if (xhttp.readyState == 4 && xhttp.status != 200) {
@@ -77,55 +76,3 @@ function updateRow(data, studio_id){
     }
 }
 
-addRowToTable = (data) => {
-
-    // Get a reference to the current table on the page and clear it out.
-    let currentTable = document.getElementById("studios-table");
-
-    // Get the location where we should insert the new row (end of table)
-    let newRowIndex = currentTable.rows.length;
-
-    // Get a reference to the new row from the database query (last object)
-    let parsedData = JSON.parse(data);
-    let newRow = parsedData[parsedData.length - 1]
-
-    // Create a row and 4 cells
-    let row = document.createElement("TR");
-    let studioIDCell = document.createElement("TD");
-    let studioNameCell = document.createElement("TD");
-    let yearFoundedCell = document.createElement("TD");
-
-    let deleteCell = document.createElement("TD");
-
-    // Fill the cells with correct data
-    studioIDCell.innerText = newRow.studio_id;
-    studioNameCell.innerText = newRow.studio_name;
-    yearFoundedCell.innerText = newRow.year_founded;
-
-    deleteCell = document.createElement("button");
-    deleteCell.innerHTML = "Delete";
-    deleteCell.onclick = function(){
-        deleteStudio(newRow.studio_id);
-    };
-
-    // Add the cells to the row 
-    row.appendChild(studioIDCell);
-    row.appendChild(studioNameCell);
-    row.appendChild(yearFoundedCell);
-    row.appendChild(deleteCell);
-
-    // Add a custom row attribute so the deleteRow function can find a newly added row
-    row.setAttribute('data-value', newRow.studio_id);
-
-    // Add the row to the table
-    currentTable.appendChild(row);
-
-    // Find drop down menu, create a new option, fill data in the option (full name, id),
-    // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
-    let selectMenu = document.getElementById("mySelect");
-    let option = document.createElement("option");
-    option.text = newRow.studio_name;
-    option.value = newRow.studio_id;
-    selectMenu.add(option);
-    // End of new step 8 code.
-}
