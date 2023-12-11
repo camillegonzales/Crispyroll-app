c98e55d604a0712e833503b66c40cda68e705ce1
jct102 <122237723+jct102@users.noreply.github.com>
2023-12-11 15:32:15 -0500

    remove user and pass for db for submission

--- database/db-connector.js
+++ database/db-connector.js
@@ -14,8 +14,8 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_gonzcami',
-    password        : '3600',
+    user            : '',
+    password        : '',
     database        : 'cs340_gonzcami'
 })
 
