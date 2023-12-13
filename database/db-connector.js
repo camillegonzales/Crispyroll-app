c88c4db0fc53a3e8d71d06ab97d3b02feaad189c
trainorj <trainorj@oregonstate.edu>
2023-12-13 12:23:55 -0500

    edits

---
+++ database/db-connector.js
@@ -0,0 +1,23 @@
+// Database Connector
+
+/*  
+  Citation for the following setup: 
+    Date: 11/14/2023
+    Based on the CS340 starter code
+    Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app 
+*/
+
+// Get an instance of mysql we can use in the app
+var mysql = require('mysql')
+
+// Create a 'connection pool' using the provided credentials
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : '',
+    password        : '',
+    database        : ''
+})
+
+// Export it for use in our applicaiton
+module.exports.pool = pool;
