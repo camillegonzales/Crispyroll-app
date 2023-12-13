c452bbcc4000b51418ac3a8f97212b45b42043ce
trainorj <trainorj@oregonstate.edu>
2023-12-13 12:37:14 -0500

    edits

--- database/db-connector.js
+++ database/db-connector.js
@@ -14,9 +14,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : '',
-    password        : '',
-    database        : ''
+    user            : 'cs340_gonzcami',
+    password        : '3600',
+    database        : 'cs340_gonzcami'
 })
 
 // Export it for use in our applicaiton
